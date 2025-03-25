060cf9aa28f8796c6ed6cee47891d05e5fb5fe17
nageshbhagelli <bhagellinagesh@gmail.com>
2025-03-25 18:21:07 +0530

    files updated

---
+++ Queue.java
@@ -0,0 +1,12 @@
+/*
+ * What is a Queue?
+ * A queue is a linear data structure which models real world queues by having
+ * two primary operations,
+ * namely enqueue and dequeue.
+ * 
+ * When and where is a Queue used?
+ * -any waiting line models a queue, for example: a lineup at a movie counter
+ * -can be used to efficiently keep track of the x most recently added elements
+ * -web server request management where you want first come first serve
+ * -can be used to do a Breadth First Search(BFS) on a graph
+ */
---
+++ Stack.java
@@ -0,0 +1,14 @@
+/*
+ * What is a Stack?
+ * A stack is a one-ended linear data structure which models a real world stack
+ * by having two primary
+ * operations, namely push and pop.
+ * 
+ * When and where is a stack used?
+ * -used by undo mechanisms in text editors.
+ * -used in compiler syntax checking for matching brackets and braces
+ * -can be used to model a pile of books or plates
+ * -used behind the scenes to support recursion by keeping track of previous
+ * function calls
+ * -can be used to do a Depth First Search(DFS) on a graph
+ */
